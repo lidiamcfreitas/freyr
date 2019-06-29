07dca4195d978af6dde8d847ed3a89b3edbb27ba
João Miranda de Almeida <jmirandadealme@gmail.com>
2019-06-29 19:52:00 +0200

    Also read channel 1 and 2

--- ADCRead.py
+++ ADCRead.py
@@ -18,7 +18,15 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
 
+chan_1 = AnalogIn(mcp, MCP.P1)
+
+chan_2 = AnalogIn(mcp, MCP.P2)
+
 while True:
-    print('Raw ADC Value: ', chan.value)
-    print('ADC Voltage: ' + str(chan.voltage) + 'V')
+    print('Channel 0 Raw ADC Value: ', chan.value)
+    print('Channel 0 ADC Voltage: ' + str(chan.voltage) + 'V')
+    print('Channel 1 Raw ADC Value: ', chan_1.value)
+    print('Channel 1 ADC Voltage: ' + str(chan_1.voltage) + 'V')
+    print('Channel 2 Raw ADC Value: ', chan_2.value)
+    print('Channel 2 ADC Voltage: ' + str(chan_2.voltage) + 'V')
     sleep(1)
