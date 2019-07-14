b4c8cdf36c66871ca9be729be246e15ff4d9a3ec
João Miranda de Almeida <jmirandadealme@gmail.com>
2019-07-14 16:04:43 +0200

    Make monitor DHT 11 robust to kill signals

--- s_dht11.py
+++ s_dht11.py
@@ -1,4 +1,6 @@
+import signal
 from time import sleep
+
 import board
 import adafruit_dht
 from datetime import datetime
@@ -14,16 +16,31 @@ SLEEP_SECONDS_SENSORS = args.sleep
 
 dhtDevice = adafruit_dht.DHT11(board.D18)
 
-current_day = datetime.now().day
-out_path = "s_dht11_{}_out.csv".format(datetime.now().strftime('%Y_%m_%d'))
-out_file = open(out_path, 'a')
+
+class DHT11Monitor:
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.terminate)
+        signal.signal(signal.SIGTERM, self.terminate)
+        self.current_day = datetime.now().day
+        self.out_path = "s_dht11_{}_out.csv".format(datetime.now().strftime('%Y_%m_%d'))
+        self.out_file = open(self.out_path, 'a')
+
+    def terminate(self):
+        self.out_file.close()
+        print("Exiting ... ")
+
+    def change_log_file(self):
+        self.out_file.close()
+        self.out_path = "s_dht11_{}_out.csv".format(datetime.now().strftime('%Y_%m_%d'))
+        self.out_file = open(self.out_path, 'a')
+        self.current_day = datetime.now().day
+
+
+monitor = DHT11Monitor()
 
 while True:
-    if datetime.now().day != current_day:
-        out_file.close()
-        out_path = "s_dht11_{}_out.csv".format(datetime.now().strftime('%Y_%m_%d'))
-        out_file = open(out_path, 'a')
-        current_day = datetime.now().day
+    if datetime.now().day != monitor.current_day:
+        monitor.change_log_file()
 
     for _ in range(5):
         try:
@@ -34,7 +51,7 @@ while True:
             print("{} - Temperature: {:.1f}C    Humidity: {}% ".format(
                 now.strftime('%Y_%m_%d_%H%M%S'), temperature_c, humidity), flush=True)
             to_write = "{}, {:.1}, {}\n".format(now.strftime('%Y_%m_%d_%H%M%S'), temperature_c, humidity)
-            out_file.write(to_write)
+            monitor.out_file.write(to_write)
             break
         except RuntimeError as error:
             pass
