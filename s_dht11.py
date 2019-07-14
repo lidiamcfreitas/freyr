79e21fcadf6fb966fbc74370d62f61d48a65daca
João Miranda de Almeida <jmirandadealme@gmail.com>
2019-07-14 11:03:29 +0200

    Print results to .csv file for dht11

--- s_dht11.py
+++ s_dht11.py
@@ -14,14 +14,27 @@ SLEEP_SECONDS_SENSORS = args.sleep
 
 dhtDevice = adafruit_dht.DHT11(board.D18)
 
+current_day = datetime.now().day
+out_path = "s_dht11_{}_out.csv".format(datetime.now().strftime('%Y_%m_%d'))
+out_file = open(out_path, 'a')
+
 while True:
+    if datetime.now().day != current_day:
+        out_file.close()
+        out_path = "s_dht11_{}_out.csv".format(datetime.now().strftime('%Y_%m_%d'))
+        out_file = open(out_path, 'a')
+        current_day = datetime.now().day
+
     for _ in range(5):
         try:
             now = datetime.now()
             temperature_c = dhtDevice.temperature
             humidity = dhtDevice.humidity
+
             print("{} - Temperature: {:.1f}C    Humidity: {}% ".format(
                 now.strftime('%Y_%m_%d_%H%M%S'), temperature_c, humidity), flush=True)
+            to_write = "{}, {:.1}, {}\n".format(now.strftime('%Y_%m_%d_%H%M%S'), temperature_c, humidity)
+            out_file.write(to_write)
             break
         except RuntimeError as error:
             pass
